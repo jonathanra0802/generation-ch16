4d421cf8d5f9598a2c0cef582e8f6953b567f151
JonathanRA <jralonso1985@gmail.com>
2022-08-16 16:29:59 -0500

    Introduccion a Java con IDEA Intelligent

---
+++ 5-java/java-01/Hola.java
@@ -0,0 +1,12 @@
+public class Hola {
+// Esto es una clase de JDK de  java 
+//String miVariable = "Hola mundo java";
+
+	public static void  main (String arg []){
+	//String miVariable ="Hola mundo java saluda a: ";
+	String miNombre ="Jonathan Ramirez";
+
+	System.out.println("Hola saludos a: "+ miNombre);
+	}
+	//Es la declaracion del metodo main
+}
---
+++ 5-java/java-01/prueba5/src/main/java/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        //String miVariable= "Hola mundo Java";
+        String miNombre="Jonathan Ramirez Alonso";
+        System.out.println("Saludos desde Java a: "+miNombre);
+    }
+}
