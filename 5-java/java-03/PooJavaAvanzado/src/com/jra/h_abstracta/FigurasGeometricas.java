44b475480d0dc7342df1c96571cedbafa83244ca
JonathanRA <jralonso1985@gmail.com>
2022-08-29 17:34:18 -0500

    PooAvanzado y abstract

---
+++ 5-java/java-03/PooJavaAvanzado/src/com/jra/h_abstracta/FigurasGeometricas.java
@@ -0,0 +1,7 @@
+package com.jra.h_abstracta;
+
+public abstract class FigurasGeometricas {
+    public abstract float area();
+
+
+}
--- 5-java/java-03/PooJavaAvanzado/src/com/jra/principal/EjemploHerencia.java
+++ 5-java/java-03/PooJavaAvanzado/src/com/jra/principal/EjemploHerencia.java
@@ -10,11 +10,15 @@ public class EjemploHerencia {
         Hija h=new Hija();
         h.visualizarA();
         Hijo ho=new Hijo();
+        ho.setA(15261);
         ho.visualizarABC();
 
         BasePadre obj = new BasePadre();
         obj.setA(2357);
+        Object miObj=new BasePadre();
 
-        System.out.println("El valor es: "+h.getA());
+
+        System.out.println("Atributo de la clase padre -> "+obj.getA());
+        System.out.println("Atributo de la clase hijo-> "+ ho.getA());
     }
 }
